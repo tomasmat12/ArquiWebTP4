c7c69aa5b305a24a3987c479ad9ed9ce30757730
Matias Tomas <tomasrimolimatias@gmail.com>
2021-11-02 19:53:57 -0300

    Iniciando TP

---
+++ src/main/java/ArquiWeb/Spring/demo/ArquiWebTp4Application.java
@@ -0,0 +1,13 @@
+package ArquiWeb.Spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiWebTp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiWebTp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ArquiWeb/Spring/demo/ArquiWebTp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ArquiWeb.Spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiWebTp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
